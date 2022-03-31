274facb1b5d96004273611af92b1481d1446a29f
huiliangShen <alal2al@hotmail.com>
2022-03-31 12:03:39 +0800

    添加一些注解

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ export default class QuickLRU extends Map {
 	 * // 过期时间
 	 * maxAge: number;
 	 * // 在从缓存中逐出项之前调用。对于副作用或需要显式清理的对象URL（“revokeObjectURL”）之类的项目非常有用。???
-	 * onEviction: (key: KeyType, value: ValueType) => void;
+	 * onEviction?: (key: KeyType, value: ValueType) => void;
 	 * }
 	 * @param {*} options 
 	 */
@@ -41,48 +41,85 @@ export default class QuickLRU extends Map {
 			this.onEviction(key, item.value);
 		}
 	}
-
+	/**
+	 * 删除过期元素
+	 * @param {*} key 
+	 * @param {*} item 
+	 * @returns false | any 
+	 */
 	_deleteIfExpired(key, item) {
+		// 过期执行删除操作并执行onEviction
 		if (typeof item.expiry === 'number' && item.expiry <= Date.now()) {
 			if (typeof this.onEviction === 'function') {
 				this.onEviction(key, item.value);
 			}
-
+			// 会返回value
 			return this.delete(key);
 		}
 
 		return false;
 	}
-
+	/**
+	 * 获取或者删除过期的内容
+	 * @param {*} key 
+	 * @param {*} item 
+	 * @returns 
+	 */
 	_getOrDeleteIfExpired(key, item) {
 		const deleted = this._deleteIfExpired(key, item);
+		// false 为 不过期
 		if (deleted === false) {
 			return item.value;
 		}
 	}
-
+	/**
+	 * 
+	 * @param {*} key 
+	 * @param {*} item = {value: any, expiry?: number}
+	 * @returns undefiend | any
+	 */
 	_getItemValue(key, item) {
+		// 不存在过期时间直接返回
+		// 存在判断是否过期，过期删除不返回值，不过期返回值
 		return item.expiry ? this._getOrDeleteIfExpired(key, item) : item.value;
 	}
-
+	/**
+	 * 验证是否过期，过期undefined否则value
+	 * @param {*} key 
+	 * @param {*} cache 
+	 * @returns 
+	 */
 	_peek(key, cache) {
 		const item = cache.get(key);
 
 		return this._getItemValue(key, item);
 	}
-
+	/**
+	 * 
+	 * @param {*} key 
+	 * @param {*} value = {value: any, expiry?: number}
+	 */
 	_set(key, value) {
+		// cache内添加，并增加size
 		this.cache.set(key, value);
 		this._size++;
-
+		
 		if (this._size >= this.maxSize) {
 			this._size = 0;
+
+			// loop onEviction?.()
 			this._emitEvictions(this.oldCache);
+			// 清空cache
 			this.oldCache = this.cache;
 			this.cache = new Map();
 		}
 	}
-
+	/**
+	 * 移动到最近使用
+	 * 从oldCache内删除并添加到cache内
+	 * @param {*} key 
+	 * @param {*} item 
+	 */
 	_moveToRecent(key, item) {
 		this.oldCache.delete(key);
 		this._set(key, item);
@@ -107,38 +144,56 @@ export default class QuickLRU extends Map {
 			}
 		}
 	}
-
+	/**
+	 * 获取item
+	 * @param {*} key 
+	 * @returns any
+	 */
 	get(key) {
+		// cache内存在
 		if (this.cache.has(key)) {
 			const item = this.cache.get(key);
 
 			return this._getItemValue(key, item);
 		}
-
+		// oldCache内存在
 		if (this.oldCache.has(key)) {
 			const item = this.oldCache.get(key);
+			// false = 不过期
 			if (this._deleteIfExpired(key, item) === false) {
 				this._moveToRecent(key, item);
 				return item.value;
 			}
 		}
 	}
-
+	/**
+	 * 
+	 * @param {*} key 
+	 * @param {*} value 
+	 * @param {*} param2 = {maxAge?: number} 
+	 */
 	set(key, value, {maxAge = this.maxAge} = {}) {
 		const expiry =
 			typeof maxAge === 'number' && maxAge !== Number.POSITIVE_INFINITY ?
 				Date.now() + maxAge :
 				undefined;
+		// 判断是cache内是否存在key
+		// 存在直接覆盖
 		if (this.cache.has(key)) {
 			this.cache.set(key, {
 				value,
 				expiry
 			});
 		} else {
+			// 不存在进行新增
 			this._set(key, {value, expiry});
 		}
 	}
-
+	/**
+	 * 判断元素是否存在
+	 * @param {*}} key 
+	 * @returns 
+	 */
 	has(key) {
 		if (this.cache.has(key)) {
 			return !this._deleteIfExpired(key, this.cache.get(key));
@@ -150,7 +205,11 @@ export default class QuickLRU extends Map {
 
 		return false;
 	}
-
+	/**
+	 * 获取key对应的value
+	 * @param {*} key 
+	 * @returns 
+	 */
 	peek(key) {
 		if (this.cache.has(key)) {
 			return this._peek(key, this.cache);
@@ -160,7 +219,11 @@ export default class QuickLRU extends Map {
 			return this._peek(key, this.oldCache);
 		}
 	}
-
+	/**
+	 * 删除key对应的value
+	 * @param {*} key 
+	 * @returns 
+	 */
 	delete(key) {
 		const deleted = this.cache.delete(key);
 		if (deleted) {
@@ -169,7 +232,9 @@ export default class QuickLRU extends Map {
 
 		return this.oldCache.delete(key) || deleted;
 	}
-
+	/**
+	 * 清空
+	 */
 	clear() {
 		this.cache.clear();
 		this.oldCache.clear();
